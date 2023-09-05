8b66faba5959b3caa554d00e6da342fa9b6d142c
emmieIO <markonuoha6@gmail.com>
2023-09-05 15:24:08 +0000

    working on magic

--- 0x01-python-if_else_loops_functions/102-magic_calculation.py
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-if a < b:
-    return c
-elif c > b:
-    return a + b
-else:
-    return (a * b) - c
+def magic_calculation(a, b, c):
+    if a < b:
+        return c
+    elif c > b:
+        return a + b
+    else:
+        return (a * b) - c
