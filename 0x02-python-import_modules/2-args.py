039e3021180a6943701c37aef5d1735ba6791d1f
emmieIO <markonuoha6@gmail.com>
2023-09-07 14:03:23 +0000

    args commit

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,6 +4,9 @@ if __name__ == "__main__":
     args_len = len(argv) - 1
     if args_len == 0:
         print("{} arguments.".format(args_len))
+    elif args_len == 1:
+        print("{} argument:".format(args_len))
+        print("1: {}".format(argv[1]))
     else:
         print("{} arguments:".format(args_len))
         for args in range(1, args_len + 1):
