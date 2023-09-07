07596833c931584b0779b0ccdb1c5ef4e49275c5
emmieIO <markonuoha6@gmail.com>
2023-09-07 13:44:07 +0000

    cmd_args commit

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,6 +2,9 @@
 if __name__ == "__main__":
     from sys import argv
     args_len = len(argv) - 1
-    print("{} arguments:".format(args_len))
-    for args in range(1, args_len):
-        print("{}: {}".format(args, argv[args]))
+    if args_len == 0:
+        print("{} arguments.".format(args_len))
+    else:
+        print("{} arguments:".format(args_len))
+        for args in range(1, args_len + 1):
+            print("{}: {}".format(args, argv[args]))
