1fdd902481b6f534382ff001f123c3bd91dcc449
emmieIO <markonuoha6@gmail.com>
2023-09-07 15:19:25 +0000

    magic names commit

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -4,4 +4,4 @@ if __name__ == "__main__":
     module_names = dir(hidden_4)
     for name in module_names:
         if name[:2] != "__":
-            print(name)
+            print("{}".format(name))
