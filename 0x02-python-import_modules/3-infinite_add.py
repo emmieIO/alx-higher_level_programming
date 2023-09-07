a114efc13b1d50250696912effd1b7feb3a105a2
emmieIO <markonuoha6@gmail.com>
2023-09-07 14:18:24 +0000

    args infinite calc

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-if __name__ = "__main__":
+if __name__ == "__main__":
     from sys import argv as args
     result = 0
     args_len = len(args) - 1
     for i in range(1, args_len + 1):
         result += int(args[i])
-    print(result)
+    print("{}".format(result))
