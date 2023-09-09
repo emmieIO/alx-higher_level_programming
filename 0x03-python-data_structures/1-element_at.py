a097dba4f7560f042fa51c6917297fc93494843b
emmieIO <markonuoha6@gmail.com>
2023-09-09 06:10:58 +0000

    Task 1 correction

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    if idx > len(my_list):
+    if idx < 0:
         return None
-    elif idx < 0:
+    elif idx < len(my_list):
         return None
     else:
         return int(my_list[idx])
