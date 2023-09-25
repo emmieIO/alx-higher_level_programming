52cea459a26119444145a103916cf34ed80af982
emmieIO <markonuoha6@gmail.com>
2023-09-25 14:14:30 +0000

    Mandatory__task__1

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     try:
-        formatted_value = "{:d}".format(int(value))
+        formatted_value = "{:d}".format(value)
         print(formatted_value)
         return True
     except (ValueError, TypeError):
