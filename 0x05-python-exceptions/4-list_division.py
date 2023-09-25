24a35bed858d050c1dd967a7be16ad58b3138251
emmieIO <markonuoha6@gmail.com>
2023-09-25 16:13:03 +0000

    Mandatory__task__4

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,18 @@
+def list_division(my_list_1, my_list_2, list_length):
+    result = []
+    for i in range(list_length):
+        try:
+            if i >= len(my_list_1) or i >= len(my_list_2):
+                raise IndexError("out of range")
+            numerator = my_list_1[i]
+            denominator = my_list_2[i]
+            if not isinstance(numerator, (int, float)) or not isinstance(denominator, (int, float)):
+                raise TypeError("wrong type")
+            if denominator == 0:
+                raise ZeroDivisionError("division by 0")
+            division_result = numerator / denominator
+            result.append(division_result)
+        except (ZeroDivisionError, TypeError, IndexError) as e:
+            print(e)
+            result.append(0)
+    return result
