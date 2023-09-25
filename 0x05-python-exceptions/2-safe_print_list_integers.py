b210d8c3ce98bd9fde8613009c3ad79beab64711
emmieIO <markonuoha6@gmail.com>
2023-09-25 14:55:39 +0000

    Mandatory__task__2

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -2,16 +2,19 @@
 def safe_print_list_integers(my_list=[], x=0):
     try:
         count = 0
-        index = 0
-        while count < x:
-            value = my_list[index]
-            if isinstance(value, int):
-                formatted_value = "{:d}".format(value)
-                print(formatted_value, end='')
+        for value in my_list:
+            if count >= x:
+                break
+            
+            try:
+                integer_value = int(value)
+                print("{:d}".format(integer_value), end=' ')
                 count += 1
-            index += 1
-        print()
+            except (ValueError, TypeError):
+                continue  # Ignore non-integer values
+            
+        print()  # Print a newline to end the line
+        return count
+    except TypeError:
+        raise TypeError("Input list must be iterable")
 
-        return count
-    except IndexError:
-        return count
