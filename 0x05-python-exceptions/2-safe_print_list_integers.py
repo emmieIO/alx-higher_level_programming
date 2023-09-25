62cc259de105a3ef3b107ebbe9e9f332956779dd
emmieIO <markonuoha6@gmail.com>
2023-09-25 15:08:26 +0000

    Mandatory__task__2

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -7,6 +7,6 @@ def safe_print_list_integers(my_list=[], x=0):
                 print("{:d}".format(item), end=' ')
                 count += 1
         print()
-    except IndexError:
+    except (IndexError, TypeError, ValueError):
         print("Error: Index out of range")
-    return count
+    return(count)
