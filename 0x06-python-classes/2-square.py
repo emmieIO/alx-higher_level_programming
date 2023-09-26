cb4663a98da9eda758defae7aabe7e9f22a49d2f
emmieIO <markonuoha6@gmail.com>
2023-09-26 12:01:46 +0000

    Mandatory__tasks0-3

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
+"""Define a class Square."""
+
+
 class Square:
-    """Square class """
-    def __init__(self, size=0):
-        """Constructor initialization"""
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
+    """Represent a square."""
+
+    def __init__(self, size):
+        """Initialization a new Square.
+
+        Params:
+            size (int): The size of the new square.
+        """
         self.__size = size
