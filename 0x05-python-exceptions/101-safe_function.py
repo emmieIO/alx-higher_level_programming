1d719acd340309635a572dc282b0d8f85cddd594
emmieIO <markonuoha6@gmail.com>
2023-09-26 01:17:07 +0000

    Advanced__task__8

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -6,6 +6,6 @@ def safe_function(fct, *args):
     try:
         function = fct(*args)
         return (function)
-    except (ValueError, TypeError):
+    except Exception:
         print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
         return (None)
