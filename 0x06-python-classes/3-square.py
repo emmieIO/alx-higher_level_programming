d877b811b0b55bd4ba21c540d00a1de07ace11f4
emmieIO <markonuoha6@gmail.com>
2023-09-26 13:24:51 +0000

    Mandatory__tasks0-3

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
+"""Square Class."""
 
 class Square:
+     """main square class."""
     def __init__(self, size=0):
+        """init constructor"""
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
@@ -9,4 +12,5 @@ class Square:
         self.__size = size
 
     def area(self):
+        """Area of square"""
         return (self.__size * self.__size)
