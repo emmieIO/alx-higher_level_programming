7ffe9a8cb847883a3529f261c7fcd026f5b6f0e0
emmieIO <markonuoha6@gmail.com>
2023-09-28 23:31:30 +0000

    mandatory --rotl

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,24 @@
 #!/usr/bin/python3
+
 class Square:
+    """
+    A class representing a square with a given size.
+
+    Attributes:
+        __size (int): The size of the square, which is a private attribute.
+
+    Methods:
+        __init__(self, size):
+            Initializes a new Square object with the specified size.
+
+    """
+
     def __init__(self, size):
+        """
+        Initializes a new Square object with the specified size.
+
+        Args:
+            size (int): The size of the square.
+
+        """
         self.__size = size
