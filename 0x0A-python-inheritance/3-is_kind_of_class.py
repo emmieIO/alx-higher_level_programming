4fe8bac98f9f6fc0b30e6a8c9d03c195950075e0
emmieIO <markonuoha6@gmail.com>
2023-10-09 22:20:51 +0000

    task 4

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""kind of class"""
+
+
+def is_kind_of_class(obj, a_class):
+    """Representation of class """
+    if isinstance(obj, a_class):
+        return True
+    return False
