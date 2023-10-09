f0195683387dd3ee0ec00efe2431e12cc5294c0e
emmieIO <markonuoha6@gmail.com>
2023-10-09 21:30:37 +0000

    task 2

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+#!/usr/bin/python3
+"""MyList Class """
+
+
+class MyList(list):
+    """Implementation of MyList class."""
+
+    def print_sorted(self):
+        """Prints a sorted list in ascending order"""
+        print(sorted(self))
