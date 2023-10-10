7b335a821223675c7803320798989b4320de0e73
emmieIO <markonuoha6@gmail.com>
2023-10-10 21:57:01 +0000

    my_file_0.txt

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Define Read file function"""
+
+
+def read_file(filename=""):
+    "Representation of read file function"
+    with open(filename, 'r') as f:
+        data = f.read()
+        print(data);
