66329303ed25ef6e7224ecf21821c19372b5b37c
emmieIO <markonuoha6@gmail.com>
2023-12-13 13:51:19 +0100

    7-cities

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,13 @@
+-- Create or use the database hbtn_0d_usa
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+-- Switch to the hbtn_0d_usa database
+USE hbtn_0d_usa;
+
+-- Create or use the table cities
+CREATE TABLE IF NOT EXISTS cities (
+    id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    state_id INT NOT NULL,
+    name VARCHAR(256) NOT NULL,
+    FOREIGN KEY (state_id) REFERENCES states(id)
+);
