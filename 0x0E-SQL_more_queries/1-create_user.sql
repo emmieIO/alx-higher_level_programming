8e6b8001a42f10ed41897b65546033e66a32b8d6
emmieIO <markonuoha6@gmail.com>
2023-12-13 12:34:21 +0100

    pushing task 0 & 1

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,4 +1,3 @@
 -- Create User
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED 
-WITH authentication_plugin BY 'user_0d_1_pwd';
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
