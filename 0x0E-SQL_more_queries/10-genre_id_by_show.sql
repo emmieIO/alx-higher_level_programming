214f946d1990959a9dd406b539105759f3ee2f3d
emmieIO <markonuoha6@gmail.com>
2023-12-13 15:43:21 +0100

    Join city by state

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,7 +1,7 @@
 -- List all shows with at least one linked genre
-USE hbtn_0d_tvshows;
-SELECT DISTINCT tv_shows.title, tv_show_genres.genre_id
-FROM tv_shows
-LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-WHERE tv_show_genres.genre_id IS NOT NULL
-ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
+SELECT DISTINCT ts.title, tsg.genre_id
+FROM tv_shows AS ts
+    LEFT JOIN tv_show_genres As tsg
+    ON ts.id = tsg.show_id
+    WHERE tsg.genre_id IS NOT NULL
+ORDER BY ts.title ASC, tsg.genre_id ASC;
