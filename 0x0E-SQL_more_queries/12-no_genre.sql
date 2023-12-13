6076c9ef7cf8c950e68051c9786c2a6d3b3b2de9
emmieIO <markonuoha6@gmail.com>
2023-12-13 15:57:48 +0100

    8-cities_of_california

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -2,5 +2,6 @@
 SELECT DISTINCT ts.title, tsg.genre_id
 FROM tv_shows AS ts
     LEFT JOIN tv_show_genres As tsg
-    ON ts.id = NULL
+    ON ts.id = tsg.show_id
+    WHERE tsg.genre_id IS NULL
 ORDER BY ts.title ASC, tsg.genre_id ASC;
