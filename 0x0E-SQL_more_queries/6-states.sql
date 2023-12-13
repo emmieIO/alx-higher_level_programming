9070524de53b935f7d87b1ef5e1177001c486630
emmieIO <markonuoha6@gmail.com>
2023-12-13 13:37:42 +0100

    6-states

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,9 @@
+-- 6-states.sql
+-- Create or use the database hbtn_0d_usa
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa
+-- Create or use the table states
+CREATE TABLE IF NOT EXISTS states (
+    id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    name VARCHAR(256) NOT NULL
+);
