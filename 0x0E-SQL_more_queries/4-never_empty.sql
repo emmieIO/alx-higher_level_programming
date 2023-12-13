833baec27459dd13bd2bc4a7a0c8843834df5692
emmieIO <markonuoha6@gmail.com>
2023-12-13 13:18:37 +0100

    id_not_null

--- 0x0E-SQL_more_queries/4-never_empty.sql
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -1,2 +1,2 @@
 -- 4-never_empty.sql
-CREATE TABLE IF NOT EXISTS id_not_null(id INT NOT NULL DEFAULT=1, name VARCHAR(256))
+CREATE TABLE IF NOT EXISTS id_not_null(id INT NOT NULL DEFAULT 1, name VARCHAR(256))
