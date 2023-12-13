ea7740a9748a2ce35ead6f1066231b5e429f3256
emmieIO <markonuoha6@gmail.com>
2023-12-13 14:35:37 +0100

    8-cities_of_california

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,6 +1,5 @@
 -- 8-cities_of_california_subquery.sql
 USE hbtn_0d_usa;
-SELECT id, name
-FROM cities 
-WHERE state_id IN (SELECT id FROM states WHERE name = 'California')
+
+SELECT id, name FROM cities WHERE state_id IN (SELECT id FROM states WHERE name = 'California')
 ORDER BY id;
