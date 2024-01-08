2638d417403b73f1506600f4c5495b759b99c789
emmieIO <markonuoha6@gmail.com>
2024-01-09 00:50:15 +0100

    warmup task3

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-const numArgs = process.argv.length - 2;
-if (numArgs === 0) {
+const numArgs = process.argv.length;
+if (numArgs === 2) {
   console.log('No argument');
-} else if (numArgs === 1) {
+} else if (numArgs === 3) {
   console.log('Argument found');
 } else {
   console.log('Arguments Found');
