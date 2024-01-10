072ac6ec53e1484aadc4b427472f2e33ddc67638
emmieIO <markonuoha6@gmail.com>
2024-01-10 02:06:00 +0100

    1-9

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const list = require('./tests/100-data').list;
+const list = require('./100-data').list;
 
 console.log(list);
 console.log(list.map((item, index) => item * index));
