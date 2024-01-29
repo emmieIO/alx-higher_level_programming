80d1760d4f15d600674ce2a2785abeaec4a866b2
emmieIO <markonuoha6@gmail.com>
2024-01-29 16:20:27 +0100

    Completion of second task

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-"""0-hbtn_status.py """
+"""0-hbtn_header.py"""
+import sys
 import urllib.request
 
 
-url = "https://alx-intranet.hbtn.io/status"
+url = sys.argv[1]
 reguest = urllib.request.Request(url)
 with urllib.request.urlopen(reguest) as response:
-    x_request_id = response.headers.get()
+    x_request_id = response.headers.get("X-Request-Id")
     print(x_request_id)
