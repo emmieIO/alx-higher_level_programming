6562c8db71cf4eb91642035a73ca87dc9751d001
emmieIO <markonuoha6@gmail.com>
2024-01-29 17:09:11 +0100

    Completion of third task

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
-"""0-hbtn_header.py"""
+"""2-post_email.py"""
 import sys
 import urllib.request
 import urllib.parse
 
-url = sys.argv[1]
-email = sys.argv[2]
-data = urllib.parse.urlencode({
-    "email" : email
-}).encode("utf-8")
-reguest = urllib.request.Request(url, data)
-with urllib.request.urlopen(reguest) as response:
-    x_request_id = (dict(response.headers).get("X-Request-Id"))
-    print(x_request_id)
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
+    data = urllib.parse.urlencode({
+        "email" : email
+    }).encode("utf-8")
+    request = urllib.request.Request(url, data)
+    with urllib.request.urlopen(request) as response:
+        print(response.read().decode('utf-8'))
