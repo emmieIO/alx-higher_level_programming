b6c6de6a67ee7f808171529f6f89416b9f86beb5
emmieIO <markonuoha6@gmail.com>
2024-01-29 19:37:03 +0100

    Completion of eight  task

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""10-my_github"""
+import sys
+import requests
+from requests.auth import HTTPBasicAuth
+
+
+if __name__ == "__main__":
+    auth = HTTPBasicAuth(sys.argv[1], sys.argv[2])
+    response = requests.get("https://api.github.com/user", auth=auth)
+    print(r.json().get("id"))
+    
