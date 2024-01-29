b1809041854fbc7c00b852366b3a67819529d33e
emmieIO <markonuoha6@gmail.com>
2024-01-29 17:12:32 +0100

    Completion of third task

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     url = sys.argv[1]
     email = sys.argv[2]
     data = urllib.parse.urlencode({
-        "email" : email
+        "email": email
     }).encode("utf-8")
     request = urllib.request.Request(url, data)
     with urllib.request.urlopen(request) as response:
