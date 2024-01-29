27e39c711cb1c61c1db810ff0c26e532cca77b8b
emmieIO <markonuoha6@gmail.com>
2024-01-29 19:45:16 +0100

    Completion of eight  task

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python3
-"""2-post_email.py"""
+"""
+Sends a request to a given URL.
+"""
 import sys
 import requests
 
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    response = requests.get(url)
-    print("Error code: ", response.status_code)
+
+    r = requests.get(url)
+    if r.status_code >= 400:
+        print("Error code: {}".format(r.status_code))
+    else:
+        print(r.text)
