7fa6fa182aa598f8a2f9948661172bbf4de1c580
emmieIO <markonuoha6@gmail.com>
2024-01-29 16:23:54 +0100

    Completion of second task

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -7,5 +7,5 @@ import urllib.request
 url = sys.argv[1]
 reguest = urllib.request.Request(url)
 with urllib.request.urlopen(reguest) as response:
-    x_request_id = response.headers.get("X-Request-Id")
+    x_request_id = (dict(response.headers).get("X-Request-Id"))
     print(x_request_id)
